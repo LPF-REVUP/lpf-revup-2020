dea06a9ac158f1c4d6d3f9cf871962887c5f68aa
unitOtk <ohtsukahideaki@gmail.com>
2020-07-04 14:24:22 +0900

    improve query to connpass api

--- functions/src/connpass.ts
+++ functions/src/connpass.ts
@@ -1,9 +1,11 @@
 import axios from 'axios'
+import qs from 'qs'
 import { APIGatewayProxyEvent } from 'aws-lambda'
 
 export async function handler(event: APIGatewayProxyEvent) {
   const axiosResponse = await axios.get('https://connpass.com/api/v1/event', {
-    params: event.queryStringParameters
+    params: event.queryStringParameters,
+    paramsSerializer: params => qs.stringify(params, { arrayFormat: "repeat" })
   })
 
   return {
