b523cde9e9a1fce05cb78faa7f2054da1d57dc35
sumihiro3 <sumihiro@gmail.com>
2020-07-16 21:51:57 +0900

    remove duplicated section header

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -43,9 +43,6 @@
         v-row.section-header(id="sponsors" cols="12")
           div
             div.ml-2.text-h6.text-md-h3.font-weight-black.section-header-text.text-left.font-biryani SPONSORS
-        v-row.section-header(id="sponsors" cols="12")
-          div
-            div.ml-2.text-h6.text-md-h3.font-weight-black.section-header-text.text-left SPONSORS
             div.ml-6.text-subtitle-2.text-md-subtitle-2.section-header-text スポンサー
             sponsor-List(
               :sponsors="sponsors"
