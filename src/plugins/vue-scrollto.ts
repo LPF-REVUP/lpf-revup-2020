5a7b8f0b83856614bf76b184c1d826943f05b674
sumihiro3 <sumihiro@gmail.com>
2020-07-28 17:50:24 +0900

    change as ts

--- src/plugins/vue-scrollto.ts
+++ src/plugins/vue-scrollto.ts
@@ -4,6 +4,6 @@ import VueScrollTo from 'vue-scrollto'
 Vue.use(VueScrollTo)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function(ctx, inject) {
+export default function(ctx: any, inject: any) {
   inject('scrollTo', VueScrollTo.scrollTo)
 }
